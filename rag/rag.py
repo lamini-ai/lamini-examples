fe0d2fd0ada19f3d4087b37d37b5ccec8dad3568
Brianna Ren <briannaren@Gregs-MacBook-Pro.local>
2023-12-11 00:59:54 -0800

    remove file

--- rag/rag.py
+++
@@ -1,8 +0,0 @@
-from llama import RetrievalAugmentedRunner
-
-llm = RetrievalAugmentedRunner(chunk_size=512, step_size=256, k=5)
-llm.load_data("data")
-llm.train()
-prompt = "Have we invested in any generative AI companies in 2023?"
-response = llm(prompt)
-print(response)
