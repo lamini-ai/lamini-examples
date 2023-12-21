d753c521d2f682af20c6ebb3bb9c0dcaadc345d1
Brianna Ren <briannaren@Gregs-MacBook-Pro.local>
2023-12-21 00:24:08 -0800

    update

--- code/rag.py
+++ code/rag.py
@@ -1,6 +1,6 @@
 from retrieval_augmented_runner import RetrievalAugmentedRunner
 
-llm = RetrievalAugmentedRunner(chunk_size=512, step_size=256, k=5)
+llm = RetrievalAugmentedRunner(chunk_size=512, step_size=256)
 llm.load_data("data")
 llm.train()
 prompt = "Have we invested in any generative AI companies in 2023?"
