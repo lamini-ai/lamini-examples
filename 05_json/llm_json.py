cd6ede402ee4884838575167b94630686b22a0b2
James Wong <james@powerml.co>
2024-05-14 10:53:30 -0700

    Update Llama 3

--- 05_json/llm_json.py
+++ 05_json/llm_json.py
@@ -1,6 +1,6 @@
 from lamini import Lamini
 
-llm = Lamini(model_name="meta-llama/Llama-2-7b-chat-hf")
+llm = Lamini(model_name="meta-llama/Meta-Llama-3-8B-Instruct")
 llm.generate(
     "How old are you?",
     output_type={"age": "int", "units": "str"}
