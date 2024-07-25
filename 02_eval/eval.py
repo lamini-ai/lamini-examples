e76a01bc2d8e9b143fc37dc0f1a85c764ac99692
Ryan Compton <ryan@powerml.co>
2024-07-24 17:07:15 -0700

    Updated docstrings for eval.py

--- 02_eval/eval.py
+++ 02_eval/eval.py
@@ -1,15 +1,27 @@
+from typing import AsyncGenerator, List
+
 import jsonlines
 import os
 import logging
 
-from argparse import ArgumentParser
+from argparse import ArgumentParser, Namespace
 
-from load_earnings_call_dataset import load_earnings_call_dataset
+from load_earnings_call_dataset import load_earnings_call_dataset, EarningsCallsDataset
 from lamini.generation.base_prompt_object import PromptObject
 from eval_pipeline import evaluate_model
 
 
-def main():
+def main() -> None:
+    """ Main script for eval runtime
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    None
+    """
     args = parse_arguments()
 
     setup_logging(args)
@@ -21,13 +33,53 @@ def main():
     save_results(results, args)
 
 
-async def slice_dataset(dataset, max_examples):
+async def slice_dataset(
+        dataset: EarningsCallsDataset, 
+        max_examples: int
+    ) -> AsyncGenerator[PromptObject, None, None]:
+    """ Enforce the max_examples limit on the provided
+    dataset
+
+    Parameters
+    ----------
+    dataset: EarningsCallsDataset
+        Dataset loader
+
+    max_examples: int
+        Upper limit of example count
+        
+    Yields
+    ------
+    PromptObject
+        Construct prompt from loaded example in dataset
+
+    """
+
     for index, example in enumerate(dataset):
         if index < max_examples:
             yield PromptObject(prompt=example.get_prompt(), data={"example": example})
 
 
-def parse_arguments():
+def parse_arguments() -> Namespace:
+    """ Argument Parser setup 
+    The following arguments are used in this test script:
+        --data
+            Path and file name for the evaluation data
+
+        --model
+            Name of the model to evaluate
+            default meta-llama/Meta-Llama-3-8B-Instruct
+
+        --max-examples
+            Max number of examples to evaluate
+            default 100
+        
+    Returns
+    -------
+    argparse.Namespace
+        Namespace object storing the given args as attributes
+    """
+
     parser = ArgumentParser()
 
     parser.add_argument(
@@ -54,7 +106,26 @@ def parse_arguments():
     return parser.parse_args()
 
 
-def setup_logging(args):
+def setup_logging(args: Namespace) -> None:
+    """ Establish logging configuration and starting log
+
+    Parameters
+    ----------
+    args: Namespace
+        Input arguments to the main script
+        The following values are used:
+            data
+                Path and file name for the evaluation data
+
+            model
+                Name of the model to evaluate
+                default meta-llama/Meta-Llama-3-8B-Instruct
+
+    Returns
+    -------
+    None
+    """
+
     # Remove all handlers associated with the root logger object.
     for handler in logging.root.handlers[:]:
         logging.root.removeHandler(handler)
@@ -68,14 +139,58 @@ def setup_logging(args):
     logging.info(f"Evaluating model {args.model} on dataset {args.data}")
 
 
-def load_dataset(args):
+def load_dataset(args: Namespace) -> EarningsCallsDataset:
+    """ Logic for which dataset to return. Specific loading
+    functions exist for different data key words in args.
+
+    Parameters
+    ----------
+    args: Namespace
+        Input arguments to the main script
+        The following values are used:
+            data
+                Path and file name for the evaluation data
+
+    Returns
+    -------
+    EarningsCallsDataset
+        Dataset returned from the specified loading function
+
+    Raises
+    ------
+    ValueError
+        Raised if the dataset is not found in the pre-set 
+        dataset key words
+    """
+
     if args.data == "earnings":
         return load_earnings_call_dataset()
     else:
         raise ValueError(f"Unknown dataset: {args.data}")
 
 
-def save_results(results, args):
+def save_results(results: List[PromptObject], args: Namespace) -> None:
+    """ Store results in provided path in args
+
+    Parameters
+    ----------
+    results: List[PromptObject]
+        Return list of query results to LLM
+
+    args: Namespace
+        Input arguments to the main script
+        The following values are used:
+            data
+                Path and file name for the evaluation data
+            model
+                Name of the model to evaluate
+                default meta-llama/Meta-Llama-3-8B-Instruct
+
+    Returns
+    -------
+    None
+    """
+
     base_path = "/app/lamini-earnings-sdk/data/results"
     experiment_name = f"{args.data}_{args.model}".replace("/", "_")
 
@@ -98,5 +213,5 @@ def save_results(results, args):
                 }
             )
 
-
-main()
+if __name__ == "__main__":
+    main()
