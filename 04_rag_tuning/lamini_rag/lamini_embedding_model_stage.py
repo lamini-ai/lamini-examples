cfdc330274c904220c766f4bab9b5675ec17c8cb
Yaxiong Zhao <yaxiong@lamini.ai>
2024-07-25 20:01:25 -0700

    Rewrite the other one
    Signed-off-by: Yaxiong Zhao <yaxiong@lamini.ai>

--- 04_rag_tuning/lamini_rag/lamini_embedding_model_stage.py
+++ 04_rag_tuning/lamini_rag/lamini_embedding_model_stage.py
@@ -27,42 +27,7 @@ class LaminiEmbeddingModelStage(EmbeddingNode):
         super().__init__()
         self.dataset = dataset
 
-    def generate(
-        self,
-        prompt: Union[Iterator[PromptObject], AsyncIterator[PromptObject]],
-        model_name: Optional[str] = None,
-    ):
-        """ Get the query prompt before passing to the parent class'
-        generate function.
-
-        Parameters
-        ----------
-        prompt: Union[Iterator[PromptObject], AsyncIterator[PromptObject]]
-            An iterator (Async or not) used as the input for the async call
-            of get_query_prompt to encapsulate the text for model input 
-            within a PromptObject
-
-        model_name: Optional[str]
-            Model name that will be passed to the API call
-        
-        Returns
-        -------
-        Generator: 
-            A generator is returned from the parent class' generate call, 
-            which in turn is returned from this function. For more information
-            on the generator returned, refer to:
-                https://github.com/lamini-ai/lamini/blob/main/lamini/generation/generation_node.py#L48
-                The parent EmbeddingNode of this class is itself a child of the GenerationNode,
-                which is why you should refer to GenerationNode for parent functionality
-        """
-        prompt = self.get_query_prompt(prompt)
-
-        return super().generate(
-            prompt,
-            model_name=model_name,
-        )
-
-    async def get_query_prompt(self, prompts):
+    def preprocess(self, prompt: PromptObject):
         """ A generator function that will yield the PromptObject
         built for the provided prompts in the prompts Iterator
 
@@ -79,8 +44,6 @@ class LaminiEmbeddingModelStage(EmbeddingNode):
             Object storing and building the query and associated relevant info
             for RAG inference
         """
-        async for prompt in prompts:
-            query = prompt.data["example"].get_query()
-
-            yield PromptObject(prompt=query, data=prompt.data)
+        query = prompt.data["example"].get_query()
+        return PromptObject(prompt=query, data=prompt.data)
 
