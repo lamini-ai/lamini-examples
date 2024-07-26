e354f1e165681cfcbd1c066e4fc5e2fd36747c45
Yaxiong Zhao <yaxiong@lamini.ai>
2024-07-25 20:11:47 -0700

    Fix

--- 04_rag_tuning/lamini_rag/lamini_embedding_model_stage.py
+++ 04_rag_tuning/lamini_rag/lamini_embedding_model_stage.py
@@ -13,22 +13,6 @@ class LaminiEmbeddingModelStage(EmbeddingNode):
     """
 
     def preprocess(self, prompt: PromptObject):
-        """ A generator function that will yield the PromptObject
-        built for the provided prompts in the prompts Iterator
-
-        Parameters
-        ----------
-        prompts: Union[Iterator[PromptObject], AsyncIterator[PromptObject]]
-            An iterator (Async or not) used as the input for the async call
-            of get_query_prompt to encapsulate the text for model input 
-            within a PromptObject
-        
-        Yields
-        -------
-        PromptObject: 
-            Object storing and building the query and associated relevant info
-            for RAG inference
-        """
         query = prompt.data["example"].get_query()
         return PromptObject(prompt=query, data=prompt.data)
 
