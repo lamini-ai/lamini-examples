ba858f7eaedca4242a45227c1d8d09ca5e5040d7
PowerML <powerml@PowerMLs-MBP.attlocal.net>
2024-08-02 14:17:41 -0700

    generate data for earnings

--- 05_data_pipeline/generate_data.py
+++ 05_data_pipeline/generate_data.py
@@ -1,13 +1,16 @@
-import asyncio
-import itertools
-import jsonlines
-import logging
-
-from tqdm import tqdm
-
 from lamini.generation.generation_node import GenerationNode
 from lamini.generation.generation_pipeline import GenerationPipeline
 from lamini.generation.base_prompt_object import PromptObject
+
+import jsonlines
+
+import collections
+import asyncio
+from tqdm import tqdm
+
+from typing import Union, Iterator, AsyncIterator
+
+import logging
 
 logger = logging.getLogger(__name__)
 
@@ -15,6 +18,24 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
 )
+
+
+async def main():
+    earnings_calls = load_earnings_calls()
+
+    answers = QuestionAnswerPipeline().call(earnings_calls)
+
+    await save_answers(answers)
+
+
+async def load_earnings_calls():
+    path = "/app/lamini-earnings-sdk/data/WFC_earnings_transcripts.jsonl"
+
+    with jsonlines.open(path) as reader:
+        for line in reader:
+            logger.info(f"Loaded earnings call for {line['ticker']}")
+            yield PromptObject(prompt="", data=line)
+
 
 class QuestionAnswerPipeline(GenerationPipeline):
     def __init__(self):
@@ -24,17 +45,109 @@ class QuestionAnswerPipeline(GenerationPipeline):
         self.answer_generator = AnswerGenerator()
 
     def forward(self, x):
-        """Defines the pipeline
-        
-        forward() is invoked by __call__() as shown in run_pipeline() below.
-        """
-        x = self.question_generator(x, output_type={
-            "question_1": "str",
-            "question_2": "str",
-            "question_3": "str",
-        })
+        x = self.question_generator(x)
         x = self.answer_generator(x)
         return x
+
+
+class QuestionGenerator(GenerationNode):
+    def __init__(self):
+        super(QuestionGenerator, self).__init__(
+            model_name="meta-llama/Meta-Llama-3-8B-Instruct", max_new_tokens=150
+        )
+
+    def generate(
+        self,
+        prompt: Union[Iterator[PromptObject], AsyncIterator[PromptObject]],
+        *args,
+        **kwargs,
+    ):
+        prompt = self.add_template(prompt)
+
+        results = super(QuestionGenerator, self).generate(
+            prompt,
+            output_type={
+                "question_1": "string",
+                "question_2": "string",
+                "question_3": "string",
+            },
+            *args,
+            **kwargs,
+        )
+        return results
+
+    async def process_results(self, results):
+        async for result in results:
+            logger.debug(f"Generated question for {result}")
+            if result is None:
+                continue
+
+            if "question_1" not in result.response:
+                continue
+
+            if "question_2" not in result.response:
+                continue
+
+            if "question_3" not in result.response:
+                continue
+
+            questions = (
+                result.response["question_1"],
+                result.response["question_2"],
+                result.response["question_3"],
+            )
+            for question in questions:
+                result = PromptObject(prompt=question, data=result.data.copy())
+                yield result
+
+    async def add_template(self, prompts):
+        async for prompt in prompts:
+            chunks = chunk_prompt(prompt)
+            for chunk in chunks:
+                chunk.prompt = self.make_prompt(chunk)
+                logger.info(
+                    f"Generating question for {chunk.data['ticker']}, {chunk.data['q']}"
+                )
+                yield chunk
+
+    def make_prompt(self, chunk):
+        prompt = "<|begin_of_text|><|start_header_id|>user<|end_header_id|>"
+
+        prompt += (
+            "You are a financial analyst with extensive experience at Goldman Sachs."
+        )
+        prompt += "You are reading the earnings call transcript for the following company:\n\n"
+        prompt += "====================\n\n"
+        prompt += get_company_info(chunk) + "\n"
+        prompt += "====================\n\n"
+        prompt += (
+            "You are reading the following section of the earnings call transcript:\n\n"
+        )
+        prompt += "====================\n\n"
+        prompt += chunk.data["transcript"]
+        prompt += "====================\n\n"
+        prompt += "Consider the numbers in the transscript. "
+        prompt += "Ask three questions about the numbers in the transcript that require precise answers. "
+        prompt += "Only ask questions that can be answered using the transcript."
+        prompt += "<|eot_id|>"
+        prompt += "<|start_header_id|>assistant<|end_header_id|>"
+
+        return prompt
+
+
+def chunk_prompt(prompt):
+    transcript = prompt.data["transcript"]
+    chunk_size = 4096
+    chunk_step = 2048
+
+    for i in range(0, len(transcript), chunk_step):
+        chunk = transcript[i : i + chunk_size]
+        chunked_data = prompt.data.copy()
+        chunked_data["transcript"] = chunk
+        prompt_object = PromptObject(prompt=prompt.prompt, data=chunked_data)
+
+        yield prompt_object
+
 
 def get_company_info(chunk):
     info = f"Company: {chunk.data['exchange']}\n"
@@ -43,64 +156,6 @@ def get_company_info(chunk):
     info += f"Quarter: {chunk.data['q']}\n"
     return info
 
-class QuestionGenerator(GenerationNode):
-    """GenerationNode represents a step of processing in a pipeline.
-
-    GenerationNode.__call__() is the entrypoint, which includes 3 sub-steps for each prompt:
-    1. Transform prompt: invoke self.preprocess() to transform prompt to implement
-       the semantic of this GenerationNode.
-    2. Generate: invoke self.generate() (defined in GenerationNode) to send prompt
-       to Lamini inference API, and fetch the response
-    3. Transform response: invoke self.postprocess() to transform response to implement
-       the semantic of this GenerationNode.
-
-    If you do not need to transform prompt and response, you don't need to implement
-    self.preprocess() or self.postprocess().
-    
-    You may implement self.preprocess() and self.postprocess() for your own purpose.
-    """
-    def __init__(self):
-        super(QuestionGenerator, self).__init__(
-            model_name="meta-llama/Meta-Llama-3-8B-Instruct", max_new_tokens=150
-        )
-
-    def preprocess(self, obj: PromptObject):
-        obj.prompt = self.make_prompt(obj)
-        logger.info(f"Generating question for {obj.data['ticker']}, {obj.data['q']}")
-
-    def postprocess(self, obj: PromptObject):
-        response = obj.response
-        questions = [
-            response["question_1"],
-            response["question_2"],
-            response["question_3"],
-        ]
-        for question in questions:
-            ans = PromptObject(prompt=question, data=obj.data.copy())
-            yield ans
-
-
-    def make_prompt(self, obj):
-        """This is a helper function used by self.preprocess()"""
-        prompt = (
-            "<s>[INSTR]You are a financial analyst with extensive experience at Goldman Sachs."
-        )
-        prompt += "You are reading the earnings call transcript for the following company:\n\n"
-        prompt += "====================\n\n"
-        prompt += get_company_info(obj) + "\n"
-        prompt += "====================\n\n"
-        prompt += (
-            "You are reading the following section of the earnings call transcript:\n\n"
-        )
-        prompt += "====================\n\n"
-        prompt += obj.data["transcript"]
-        prompt += "====================\n\n"
-        prompt += "Consider the numbers in the transcript. "
-        prompt += "Ask three questions about the numbers in the transcript that require precise answers. "
-        prompt += "Only ask questions that can be answered using the transcript."
-        prompt +="[/INSTR]"
-
-        return prompt
 
 class AnswerGenerator(GenerationNode):
     def __init__(self):
@@ -108,47 +163,60 @@ class AnswerGenerator(GenerationNode):
             model_name="meta-llama/Meta-Llama-3-8B-Instruct", max_new_tokens=150
         )
 
-    def postprocess(self, obj: PromptObject):
-        logger.info(f"Generated answer for {obj}")
+    def generate(
+        self,
+        prompt: Union[Iterator[PromptObject], AsyncIterator[PromptObject]],
+        *args,
+        **kwargs,
+    ):
+        prompt = self.add_template(prompt)
+        results = super(AnswerGenerator, self).generate(prompt, output_type={"answer" : "str"}, *args, **kwargs)
+        return results
 
-    def preprocess(self, obj: PromptObject):
-        obj.data["question"] = obj.prompt
-        obj.prompt = self.make_prompt(obj)
+    async def process_results(self, results):
+        async for result in results:
+            logger.info(f"Generated answer for {result}")
+            if result is None:
+                continue
+            yield result
 
-    def make_prompt(self, obj: PromptObject):
-        prompt = (
-            "<s>[INSTR] You are a financial analyst with extensive experience at Goldman Sachs."
+    async def add_template(self, prompts):
+        async for prompt in prompts:
+            logger.info(
+                f"Generating answer for {prompt.data['ticker']}, {prompt.data['q']}, {prompt.prompt}"
+            )
+            prompt.data["question"] = prompt.prompt
+            prompt.prompt = self.make_prompt(prompt)
+            yield prompt
+
+    def make_prompt(self, chunk):
+        prompt = "<|begin_of_text|><|start_header_id|>user<|end_header_id|>"
+
+        prompt += (
+            "You are a financial analyst with extensive experience at Goldman Sachs."
         )
         prompt += "You are reading the earnings call transcript for the following company:\n\n"
         prompt += "====================\n\n"
-        prompt += get_company_info(obj)
+        prompt += get_company_info(chunk)
         prompt += "====================\n\n"
         prompt += (
             "You are reading the following section of the earnings call transcript:\n\n"
         )
         prompt += "====================\n\n"
-        prompt += obj.data["transcript"] + "\n"
+        prompt += chunk.data["transcript"] + "\n"
         prompt += "====================\n\n"
         prompt += "Consider the numbers in the transcript. "
         prompt += "If the answer to the question cannot be found in the transcript, reply that you do not know. "
         prompt += "Answer the following questions about the numbers in the transcript. "
-        prompt += obj.prompt
-        prompt += "[/INSTR]"
+        prompt += chunk.prompt
+        prompt += "<|eot_id|>"
+        prompt += "<|start_header_id|>assistant<|end_header_id|>"
 
         return prompt
-    
 
-async def load_earnings_calls():
-    path = "/app/lamini-earnings-sdk/data/test_set_transcripts.jsonl"
-
-    with jsonlines.open(path) as reader:
-        # Here we only read the 1st line from the .jsonl file.
-        for line in itertools.islice(reader, 1):
-            logger.info(f"Loaded earnings call for {line['ticker']}")
-            yield PromptObject(prompt="", data=line)
 
 async def save_answers(answers):
-    path = "/app/lamini-earnings-sdk/data/results/generated_q_a.jsonl"
+    path = "/app/lamini-earnings-sdk/data/generated_q_a.jsonl"
 
     with jsonlines.open(path, "w") as writer:
         pbar = tqdm(desc="Saving answers", unit=" answers")
@@ -160,16 +228,10 @@ async def save_answers(answers):
                 "transcript": answer.data["transcript"],
                 "prompt": answer.prompt,
                 "question": answer.data["question"],
-                "answer": answer.response["output"],
+                "answer": answer.response["answer"],
             }
             writer.write(answer)
             pbar.update()
 
 
-async def run_pipeline():
-    earnings_calls = load_earnings_calls()
-    answers = QuestionAnswerPipeline().call(earnings_calls)
-    await save_answers(answers)
-
-
-asyncio.run(run_pipeline())
+asyncio.run(main())
