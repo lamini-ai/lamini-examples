f0e67ddbb1ad62c868d18c9df551f381d0024c1e
Ryan Compton <ryan@powerml.co>
2024-08-05 11:50:02 -0700

    Added docstrings

--- 12_slackbot/slack/app.py
+++ 12_slackbot/slack/app.py
@@ -1,10 +1,11 @@
-from lamini import Lamini
-
+from typing import Callable, Dict, Any
 import logging
 import json
 import re
 import requests
 import time
+
+from lamini import Lamini
 
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
@@ -13,6 +14,7 @@ from slack_bolt.adapter.socket_mode import SocketModeHandler
 with open("/app/lamini-slackbot/slack/config.json", "r") as f:
     config = json.load(f)
 
+# Retrieve the Slack token from the config
 SLACK_BOT_TOKEN = config["SLACK_BOT_TOKEN"]
 app = App(token=SLACK_BOT_TOKEN)  # Store slack bot and app token
 
@@ -23,7 +25,23 @@ model_reaction_counts = (
 logging.basicConfig(filename="/tmp/docker.log", encoding='utf-8', level=logging.DEBUG)
 
 @app.event("app_mention")
-def main_event(client, event, say):
+def main_event(client, event, say) -> None:
+    """ Main event handler for slack both integration and LLM responses
+
+    Parameters
+    ----------
+    
+
+    Returns
+    -------
+    None
+
+    Raises
+    ------
+    Exception
+        General exception catch for question answering
+    """
+
     channel_id = event["channel"]
     thread_ts = event.get("thread_ts", None) or event["ts"]
     question = re.sub("<[^>]+>", "", event["text"])  # Remove the @mention tag
@@ -83,7 +101,23 @@ def main_event(client, event, say):
 
 
 @app.event("reaction_added")
-def reaction_event(client, event):
+def reaction_event(client: object, event: Dict[str, Any]) -> None:
+    """ Handler for reaction events, handles up and down iterations
+    of the model count by the given reaction. 
+
+    Parameters
+    ----------
+    client: object
+        Slack bot client object
+    
+    event: Dict[str, Any]
+        Metadata information for the event
+
+    Returns
+    -------
+    None
+    """
+
     channel = event["item"]["channel"]
     try:
         message = client.conversations_replies(
@@ -132,7 +166,22 @@ def reaction_event(client, event):
 
 
 @app.event("reaction_removed")
-def reaction_remove_event(client, event):
+def reaction_remove_event(client: object, event: Dict[str, Any]) -> None:
+    """ Handler for a remove event
+
+    Parameters
+    ----------
+    client: object
+        Slack bot client object
+    
+    event: Dict[str, Any]
+        Metadata information for the event
+
+    Returns
+    -------
+    None
+    """
+
     channel = event["item"]["channel"]
     try:
         message = client.conversations_replies(
@@ -181,7 +230,24 @@ def reaction_remove_event(client, event):
 
 
 @app.command("/count-reactions")
-def get_count_command(ack, body, respond):
+def get_count_command(ack: Callable, body: Dict[str, Any], respond: Callable) -> None:
+    """ Return the reaction count
+
+    Parameters
+    ----------
+    ack: Callable
+        
+    body: Dict[str, Any]
+        Metadata for the channle and model 
+    
+    respond: Callable
+        Function for response generation
+    
+    Returns
+    -------
+    None
+    """
+
     ack()
     channel = body["channel_id"]
     text = ""
@@ -205,7 +271,27 @@ def get_count_command(ack, body, respond):
     )
 
 
-def ask_model_question(channel_id, model, question):
+def ask_model_question(channel_id: str, model: str, question: str) -> str:
+    """ Format and provide question the the model id given. Return the
+    response of the model or handle an error if raised.
+
+    Parameters
+    ----------
+    channel_id: str
+        Channel name
+
+    model: str
+        Model name
+
+    question: str
+        Question to be sent to the model
+
+    Returns
+    -------
+    str
+        Model response or error handling if model failed
+    """
+
     token = config["channel_token_mappings"][channel_id]["token"]
 
     system_prompt = """\
@@ -243,7 +329,20 @@ If a question does not make any sense, or is not factually coherent, explain why
         return f"Sorry I can't answer that: {response.status_code} {response.reason}"
 
 
-def post_process(answer):
+def post_process(answer: str) -> str:
+    """ Clean the provided answer
+
+    Parameters
+    ----------
+    answer: str
+        Answer from LLM
+    
+    Returns
+    -------
+    clean_answer: str
+        cleaned string
+    """
+
     clean_answer = answer.lstrip(' ')
     return clean_answer
 
