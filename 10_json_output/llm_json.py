b47a55ee68e392714301934ee507fda4936d4524
Ryan Compton <ryan@powerml.co>
2024-08-05 11:35:25 -0700

    Added single comments (#110)

--- 10_json_output/llm_json.py
+++ 10_json_output/llm_json.py
@@ -1,9 +1,13 @@
 from lamini import Lamini
 
+# Instatiate Lamini object to interface with LLMs on the compute server
 llm = Lamini(model_name="meta-llama/Meta-Llama-3-8B-Instruct")
+
+# Provide query to generate and provide output type as a dictionary
 response = llm.generate(
     "How old are you?",
     output_type={"age": "int", "units": "str"}
 )
 
+# Output response
 print(response)
