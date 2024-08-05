c169613226063e9a773cf29bb5a86b7935a4fcae
Ryan Compton <ryan@powerml.co>
2024-08-05 11:37:55 -0700

    Added single line comments

--- 11_classify/classify.py
+++ 11_classify/classify.py
@@ -1,12 +1,17 @@
 from lamini import LaminiClassifier
 
+# Instatiate Classifier object to pass prompts to the LLM on the compute server
 llm = LaminiClassifier()
 
+# Build prompt dictionary which holds the classes as the keys (correct, incorrect)
+# with the values provided descriptions for what defines the class.
 prompts={
   "correct": "Questions with correct answers.",
   "incorrect": "Questions with incorrect answers."
 }
 
+# Call prompt train to train the LLM with the provided classes and prompts
 llm.prompt_train(prompts)
 
+# Save the model for later use
 llm.save("my_model.lamini")
